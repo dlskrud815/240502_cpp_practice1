8b9d4fc8ceec031cd731db2058847867a2bc0929
dlskrud815 <dlskrud815@gmail.com>
2024-05-03 14:21:59 +0900

    Add project files.

---
+++ 240502_cpp_practice1.cpp
@@ -0,0 +1,562 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <time.h>
+#include <cmath>
+
+using namespace std;
+
+//학생 정보 구조체
+struct stdInfo
+{
+	int stdNo, stdAge, stdKoreanAge;
+	string stdName, stdBirth;
+
+	//초기화
+	stdInfo() : stdNo(0), stdAge(0), stdKoreanAge(0) {}
+};
+
+//과목 점수 구조체
+struct sbjInfo
+{
+	vector <float> sbjScore;
+	vector <string> sbjName;
+};
+
+//과목 현황 확인용 구조체
+struct scoreState
+{
+	float stdAvgScore;
+	float stdMaxScore;
+	int stdNoMaxScore;
+};
+
+//학생별 전체 성적 평균 및 석차 확인용 구조체
+struct totalRank
+{
+	float stdTotalAvg;
+	int stdTotalRnk;
+};
+
+int countSbjNum(int selectSbj)
+{
+	//정수 자리수 세기
+	int n, cnt = 0;
+
+	n = selectSbj;
+	while (n != 0)
+	{
+		n = n / 10;
+		++cnt;
+	}
+
+	return cnt;
+}
+
+void CheckBirthInput(vector <stdInfo>& stdInfoVec, int i);
+void CalStudentAgeAndInput(vector<stdInfo>& stdInfoVec, int i);
+
+void SelectOption_PrintStudentInfo(int option, vector <stdInfo> stdInfoVec, vector <sbjInfo> sbjInfoVec);
+void SelectOption_PrintStudentAgeAverage(int option, vector <stdInfo> stdInfoVec);
+void SelectOption_PrintEarlyBirthday(int option, vector <stdInfo> stdInfoVec, vector <sbjInfo> sbjInfoVec);
+void SelectOption_EditStudentInfo(int option, int editStdNum, vector <stdInfo>& stdInfoVec);
+
+void PrintStudentInfo(vector<stdInfo> stdInfoVec, int i, vector <sbjInfo> sbjInfoVec);
+void PrintSubjectStateAll(vector<scoreState> stdScoreState, vector <stdInfo> stdInfoVec, vector <sbjInfo> sbjInfoVec);
+void PrintStudentScore(vector<totalRank> stdTotalRank, vector <stdInfo> stdInfoVec, vector <sbjInfo> sbjInfoVec);
+
+vector<scoreState> SaveSubjectStateAll(vector<scoreState> stdScoreState, vector <stdInfo> stdInfoVec, vector <sbjInfo> sbjInfoVec);
+vector<totalRank> SaveTotalScore(vector<totalRank>stdTotalRank, vector <stdInfo> stdInfoVec, vector <sbjInfo> sbjInfoVec);
+int ReturnTotalRank(vector<totalRank> stdTotalRank, int i);
+
+float CalStudentAgeAverage(vector<stdInfo> stdInfoVec, bool useKoreanAge);
+
+void PrintEarlyBirthStudentInfo(vector<stdInfo> stdInfoVec, bool useYear, vector <sbjInfo> sbjInfoVec);
+
+
+int main()
+{
+	int stdNum=0, option1, option2, editStdNum, sbjNum=0, selectSbj=0;
+	char yesNo;
+	bool confirm;
+
+	vector <stdInfo> stdInfoVec; //학생 정보 구조체 벡터
+	vector <sbjInfo> sbjInfoVec; //과목 점수 구조체 벡터
+	vector <string> sbjName;
+
+	cout << "** c++ 종합 실습 **" << endl << endl;
+
+	//1. 사용자에게 몇 명의 학생을 입력할 것인지 묻기
+	cout << "몇 명의 학생 정보를 입력? ";
+	cin >> stdNum;
+
+	stdInfoVec.resize(stdNum); //학생 정보 구조체 벡터
+	sbjInfoVec.resize(stdNum); 
+
+	// 사용자에게 과목을 입력 받을 것인지 
+	cout << endl << "학생 점수도 입력? (y or n) ";
+	cin >> yesNo;
+	yesNo == 'n' ? confirm = false : confirm = true;
+
+	if (confirm)
+	{
+		cout << endl << "몇 개의 과목 점수를 입력? ";
+		cin >> sbjNum;
+		sbjName.resize(sbjNum);
+
+		for (int i = 0; i < sbjNum; i++)
+		{
+			cout << endl << "제 " << i + 1 << " 과목명은? ";
+			cin >> sbjName[i];
+		}
+	}
+
+	//2. 학생 수만큼 "이름 나이 생일" 순서로 한 번에 입력 받기
+	//나이를 입력 받지 않고 생년월일로 입력 받은 후 계산
+	for (int i = 0; i < stdNum; i++)
+	{
+		if (i == 0)
+		{
+			cout << endl << "[입력 예시]" << endl
+				<< "0번 학생" << endl
+				<< "이름: 이나경" << endl
+				<< "생년월일: 20010815" << endl;
+		}
+
+		//학생 번호 자동 순차 부여
+		stdInfoVec[i].stdNo = i + 1;
+
+		//학생 이름 입력
+		cout << endl << i + 1 << "번 학생" << endl << "이름: ";
+		cin >> stdInfoVec[i].stdName;
+
+		//학생 생년월일 여덟자리 입력
+		cout << "생년월일: ";
+		cin >> stdInfoVec[i].stdBirth;
+
+		//생년월일 자리수 및 숫자 여부 확인
+		CheckBirthInput(stdInfoVec, i);
+
+		//나이 계산 후 벡터에 저장
+		CalStudentAgeAndInput(stdInfoVec, i);
+
+		//과목 성적 입력
+		// 추가 필요 * * y or n에서 n을 선택하면 아래 항목 안 뜸! 모든 성적 0으로 할 것?
+		// 입력을 안 한 성적은 0으로 처리 -> 정보 수정부에서 정보를 수정할 수 있도록!
+		cout << endl << "* 다음 중 점수를 입력할 과목 번호를 선택해주세요 *" << endl;
+		//과목 보기 출력
+		for (int j = 0; j < sbjName.size(); j++)
+		{
+			cout << j + 1 << ") " << sbjName[j] << endl;
+		}
+
+		if (i == 0) //추가 필요 ** 나중에는 그냥 루프 돌도록 설정
+		{
+			cout << endl << "[입력예시]" << endl
+				<< "1, 2, 3 과목을 선택할 경우 " << endl
+				// 추가 필요 ** 10개 이상도 상관 없는 코드로 수정
+				<< "입력: 123" << endl << endl; //설마 과목이 10개는 안 되겠징 
+
+			cout << "입력: ";
+			cin >> selectSbj;
+
+			while (1)
+			{
+				// 추가 필요 ** 
+				if (countSbjNum(selectSbj) != sbjName.size() || countSbjNum(selectSbj) >= 10)
+				{
+					cout << endl << "* 과목의 개수가 범위를 벗어납니다." << endl
+						<< "[재입력]" << endl << "입력: ";
+					cin >> selectSbj;
+				}
+				else break;
+			}
+		}
+
+		sbjInfoVec[i].sbjName.resize(sbjName.size());
+		sbjInfoVec[i].sbjScore.resize(sbjName.size());
+
+		for (int j = 0; j < sbjName.size(); j++)
+		{
+			cout << endl << "과목 번호: " << j+1 << endl
+				<< "과목명: " << sbjName[j] << endl
+				<< "과목 점수: ";
+			sbjInfoVec[i].sbjName[j] = sbjName[j];
+			cin >> sbjInfoVec[i].sbjScore[j];
+		}
+	}
+
+	vector<scoreState> stdScoreState(sbjInfoVec[0].sbjName.size());
+	vector<totalRank>  stdTotalRank(stdInfoVec.size());
+
+	//3. 1) 학생 정보 출력, 2) 평균 나이, 3) 가장 빠른 생일, 4) 프로그램 종료 네 가지 기능 모두 구현
+	//동명이인이나 같은 생년월일 등 조회 조건이 같을 경우 모두 출력하도록 함
+	while (1)
+	{
+		cout << endl << "[옵션]" << endl
+			<< "1) 학생 정보 , 2) 평균 나이, 3) 가장 빠른 생일, 4) 프로그램 종료, 0) 입력 정보 수정, 5) 과목 현황 정보, 6) 학생 성적 확인" << endl;
+		cin >> option1;
+
+		switch (option1)
+		{
+		case 1: //1) 학생 정보
+			cout << endl << "1) 전체 학생 정보 출력, 2) 특정 학생 정보 출력" << endl;
+			cin >> option2;
+			SelectOption_PrintStudentInfo(option2, stdInfoVec, sbjInfoVec);
+			break;
+		case 2: //2) 평균 나이
+			cout << endl << "1) 만 나이 평균 출력, 2) 년 나이 평균 출력" << endl;
+			cin >> option2;
+			SelectOption_PrintStudentAgeAverage(option2, stdInfoVec);
+			break;
+		case 3: //3) 가장 빠른 생일
+			cout << endl << "1) 생일이 가장 빠른 학생 정보 출력, 2) 나이가 가장 많은 학생 정보 출력" << endl;
+			cin >> option2;
+			SelectOption_PrintEarlyBirthday(option2, stdInfoVec, sbjInfoVec);
+			break;
+		case 4: //4) 프로그램 종료
+			return 0;
+		case 5: //5) 과목 현황 정보
+			PrintSubjectStateAll(SaveSubjectStateAll(stdScoreState, stdInfoVec, sbjInfoVec), stdInfoVec, sbjInfoVec);
+			break;
+		case 6: //6) 학생 총괄 성적
+			PrintStudentScore(SaveTotalScore(stdTotalRank, stdInfoVec, sbjInfoVec), stdInfoVec, sbjInfoVec);
+			break;
+		case 0: //0) 입력 값 수정
+			cout << endl << "정보 수정을 원하는 학생 번호는.. ";
+			cin >> editStdNum;
+
+			while (1)
+			{
+				if (editStdNum <= 0 || editStdNum > stdInfoVec.size())
+				{
+					cout << endl << "* 해당 번호의 학생은 존재하지 않습니다. " << endl
+						<< endl << "[재입력]" << endl
+						<< "정보 수정을 원하는 학생 번호는.. ";
+					cin >> editStdNum;
+				}
+				else break;
+			}
+
+			cout << endl << "1) 이름 수정, 2) 생년월일 수정" << endl;
+			cin >> option2;
+			SelectOption_EditStudentInfo(option2, editStdNum, stdInfoVec);
+		}
+	}
+
+	return 0;
+}
+
+
+void CheckBirthInput(vector <stdInfo>& stdInfoVec, int i)
+{
+	while (1)
+	{
+		bool birthCheck = true;
+
+		if (stdInfoVec[i].stdBirth.size() != 8) //생년월일 여덟자리가 아닐 경우
+		{
+			cout << endl << "* 생년월일 여덟자리를 입력해주세요." << endl
+				<< "[재입력]" << endl;
+			cin >> stdInfoVec[i].stdBirth;
+		}
+		else //생년월일이 여덟자리 일 경우
+		{	//추가 필요 ** 생년월일이 날짜 형식이 맞는지 ex) 14월 90일 등 입력 불가
+			for (int j = 0; j < stdInfoVec[i].stdBirth.size(); j++) //입력한 생년월일에 대해
+			{
+				if (!isdigit(stdInfoVec[i].stdBirth[j])) //문자가 검출될 경우
+				{
+					cout << endl << "* 생년월일을 숫자로 입력해주세요." << endl
+						<< "[재입력]" << endl;
+					cin >> stdInfoVec[i].stdBirth;
+
+					birthCheck = false;
+					break;
+				}
+			}
+			if (birthCheck == true)
+			{
+				break;
+			}
+		}
+	}
+}
+
+void CalStudentAgeAndInput(vector<stdInfo>& stdInfoVec, int i)//생년월일 이용 나이 계산
+{
+	time_t timer = time(NULL);
+	struct tm* t = localtime(&timer);
+	int age, year, month, date, c_year, c_month, c_date;
+
+	year = stoi(stdInfoVec[i].stdBirth.substr(0, 4));
+	month = stoi(stdInfoVec[i].stdBirth.substr(4, 2));
+	date = stoi(stdInfoVec[i].stdBirth.substr(6, 2));
+
+	c_year = t->tm_year + 1900;
+	c_month = t->tm_mon + 1;
+	c_date = t->tm_mday;
+
+	//년 나이 계산
+	age = c_year - year;
+	stdInfoVec[i].stdKoreanAge = age + 1;
+
+	//만나이 계산
+	if (month - c_month > 0)
+	{
+		age = age - 1;
+	}
+	else if (month - c_month == 0)
+	{
+		if (date - c_date > 0)
+		{
+			age = age - 1;
+		}
+	}
+
+	stdInfoVec[i].stdAge = age;
+}
+
+
+void SelectOption_PrintStudentInfo(int option, vector <stdInfo> stdInfoVec, vector<sbjInfo> sbjInfoVec)
+{
+	string stdName;
+
+	switch (option)
+	{
+	case 1: //1) 전체 학생 정보 출력
+		for (int i = 0; i < stdInfoVec.size(); i++)
+		{
+			PrintStudentInfo(stdInfoVec, i, sbjInfoVec);
+		}
+		break;
+	case 2: //2) 특정 학생 정보 출력
+		cout << endl << "학생 이름: ";
+		cin >> stdName;
+
+		for (int i = 0; i < stdInfoVec.size(); i++)
+		{
+			if (stdInfoVec[i].stdName == stdName) //벡터 내 검색 이름 존재
+			{
+				PrintStudentInfo(stdInfoVec, i, sbjInfoVec);
+			}
+		}
+		break;
+	}
+}
+
+void SelectOption_PrintStudentAgeAverage(int option, vector <stdInfo> stdInfoVec)
+{
+	bool useKoreanAge;
+
+	switch (option)
+	{
+	case 1: //1) 만 나이 평균 출력
+		cout << endl << "학생들의 평균 만 나이는.. "
+			<< CalStudentAgeAverage(stdInfoVec, useKoreanAge = false) << "세" << endl;
+		break;
+	case 2: //2) 년 나이 평균 출력
+		cout << endl << "학생들의 평균 년 나이는.. "
+			<< CalStudentAgeAverage(stdInfoVec, useKoreanAge = true) << "세" << endl;
+		break;
+	}
+}
+
+void SelectOption_PrintEarlyBirthday(int option, vector <stdInfo> stdInfoVec, vector <sbjInfo> sbjInfoVec)
+{
+	bool useYear;
+
+	switch (option)
+	{
+	case 1: //1) 생일이 가장 빠른 학생 정보 출력
+		cout << endl << "생일이 가장 빠른 학생은.. " << endl;
+		PrintEarlyBirthStudentInfo(stdInfoVec, useYear = false, sbjInfoVec);
+		break;
+	case 2: //2) 나이가 가장 많은 학생 정보 출력
+		cout << endl << "나이가 가장 많은 학생은.. " << endl;
+		PrintEarlyBirthStudentInfo(stdInfoVec, useYear = true, sbjInfoVec);
+		break;
+	}
+}
+
+
+void SelectOption_EditStudentInfo(int option, int editStdNum, vector <stdInfo>& stdInfoVec)
+{
+	for (int i = 0; i < stdInfoVec.size(); i++)
+	{
+		if (stdInfoVec[i].stdNo == editStdNum)
+		{
+			switch (option)
+			{
+			case 1:
+				cout << endl << "이름은.. ";
+				cin >> stdInfoVec[i].stdName;
+				break;
+			case 2:
+				cout << endl << "생년월일은.. ";
+				cin >> stdInfoVec[i].stdBirth;
+
+				//생년월일 자리수 및 숫자 여부 확인
+				CheckBirthInput(stdInfoVec, i);
+				//나이 계산 후 벡터에 저장
+				CalStudentAgeAndInput(stdInfoVec, i);
+
+				break;
+			}
+		}
+	}
+}
+
+
+void PrintStudentInfo(vector<stdInfo> stdInfoVec, int i, vector<sbjInfo> sbjInfoVec)
+{
+	cout << endl << "------------- " << stdInfoVec[i].stdNo << "번 학생" << endl
+		<< "이름: " << stdInfoVec[i].stdName << endl
+		<< "년나이: " << stdInfoVec[i].stdKoreanAge << endl
+		<< "만나이: " << stdInfoVec[i].stdAge << endl
+		<< "생년월일: " << stdInfoVec[i].stdBirth << endl;
+	
+	if (sbjInfoVec[i].sbjName.size() != 0)
+	{
+		for (int j = 0; j < sbjInfoVec[i].sbjName.size(); j++)
+		{
+			cout << endl << "과목명: " << sbjInfoVec[i].sbjName[j] << endl
+				<< "과목 점수: " << sbjInfoVec[i].sbjScore[j] << endl;
+		}	
+	}
+}
+
+
+float CalStudentAgeAverage(vector<stdInfo> stdInfoVec, bool useKoreanAge)
+{
+	float ageSum = 0;
+
+	for (int i = 0; i < stdInfoVec.size(); i++)
+	{
+		ageSum += useKoreanAge ? stdInfoVec[i].stdKoreanAge : stdInfoVec[i].stdAge;
+	}
+
+	return ageSum / stdInfoVec.size();
+}
+
+
+void PrintEarlyBirthStudentInfo(vector<stdInfo> stdInfoVec, bool useYear, vector <sbjInfo> sbjInfoVec)
+{
+	int birth, minBirth = useYear ? 99991231 : 1231;
+
+	for (int i = 0; i < stdInfoVec.size(); i++)
+	{
+		birth = useYear ? stoi(stdInfoVec[i].stdBirth) : stoi(stdInfoVec[i].stdBirth.substr(4));
+
+		if (birth < minBirth)
+		{
+			minBirth = birth;
+		}
+	}
+
+	for (int i = 0; i < stdInfoVec.size(); i++)
+	{
+		birth = useYear ? stoi(stdInfoVec[i].stdBirth) : stoi(stdInfoVec[i].stdBirth.substr(4));
+
+		if (birth == minBirth)
+		{
+			PrintStudentInfo(stdInfoVec, i, sbjInfoVec);
+		}
+	}
+}
+
+vector<scoreState> SaveSubjectStateAll(vector<scoreState> stdScoreState, vector <stdInfo> stdInfoVec, vector <sbjInfo> sbjInfoVec)
+{
+	//과목별 평균 & 최고점 & 해당 학생 번호
+	for (int i = 0; i < sbjInfoVec[0].sbjName.size(); i++) //과목 개수만큼
+	{
+		float scoreSum = 0, maxScore = 0, maxStdNo = 0;
+		for (int j = 0; j < stdInfoVec.size(); j++) //학생 수만큼
+		{
+			scoreSum += sbjInfoVec[j].sbjScore[i];
+
+			if (maxScore <= sbjInfoVec[j].sbjScore[i])
+			{
+				maxScore = sbjInfoVec[j].sbjScore[i];
+				maxStdNo = j + 1;
+			}
+		}
+		stdScoreState[i].stdAvgScore = scoreSum / stdInfoVec.size();
+		stdScoreState[i].stdNoMaxScore = maxStdNo;
+		stdScoreState[i].stdMaxScore = maxScore;
+	}
+
+	return stdScoreState;
+}
+
+void PrintSubjectStateAll(vector<scoreState> stdScoreState, vector <stdInfo> stdInfoVec, vector <sbjInfo> sbjInfoVec)
+{
+	//cout << "과목별 평균 & 최고점 & 해당 학생 확인" << endl;
+	for (int i = 0; i < stdScoreState.size(); i++)
+	{
+		cout << endl << "******************** " << sbjInfoVec[0].sbjName[i]
+			<< endl << "과목 평균: " << stdScoreState[i].stdAvgScore
+			<< endl << "최고점: " << stdScoreState[i].stdMaxScore
+			<< endl << endl << "[학생 정보]";
+		
+		PrintStudentInfo(stdInfoVec, stdScoreState[i].stdNoMaxScore-1, sbjInfoVec);
+	}
+}
+
+void PrintStudentScore(vector<totalRank> stdTotalRank, vector <stdInfo> stdInfoVec, vector <sbjInfo> sbjInfoVec)
+{
+	int stdNo;
+
+	cout << endl << "성적을 확인하고자 하는 학생 번호: ";
+	cin >> stdNo;
+
+	//추가 필요 ** 학생 번호가 아닌 다른 걸 입력받았을 때
+
+	for (int i = 0; i < stdInfoVec.size(); i++)
+	{
+		if (stdNo == stdInfoVec[i].stdNo)
+		{
+			cout << endl << "------------ " << stdInfoVec[i].stdNo << "번 학생" << endl
+				<< "이름: " << stdInfoVec[i].stdName << endl
+				<< "전과목 평균: " << stdTotalRank[i].stdTotalAvg << endl
+				<< "전체 석차: " << ReturnTotalRank(stdTotalRank, i) << endl;
+		}
+	}
+}
+
+vector<totalRank> SaveTotalScore(vector<totalRank> stdTotalRank, vector <stdInfo> stdInfoVec, vector <sbjInfo> sbjInfoVec)
+{
+	//각 학생별 전체 성적 평균
+	for (int i = 0; i < stdInfoVec.size(); i++)
+	{
+		float scoreSum = 0;
+
+		for (int j = 0; j < sbjInfoVec[i].sbjScore.size(); j++)
+		{
+			scoreSum += sbjInfoVec[i].sbjScore[j];
+		}
+		stdTotalRank[i].stdTotalAvg = scoreSum / sbjInfoVec[i].sbjScore.size();
+	}
+
+	//각 학생 전체 석차
+	return stdTotalRank;
+} 
+
+int ReturnTotalRank(vector<totalRank> stdTotalRank, int i)
+{
+	int cnt = 0;
+	float cmpScore;
+
+	for (int j = 0; j < stdTotalRank.size(); j++)
+	{
+		cmpScore = stdTotalRank[j].stdTotalAvg;
+
+		if (cmpScore >= stdTotalRank[i].stdTotalAvg)
+		{
+			cnt++;
+		}
+	}
+
+	return cnt;
+}
